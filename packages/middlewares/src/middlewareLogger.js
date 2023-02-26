44c3d6b07c9fcdaf3f99e38deaaa259facec0f51
Khaaz <khaaz.dev@gmail.com>
2023-02-26 14:45:23 +0100

    fix: middleware to be case-insensitive

--- packages/middlewares/src/middlewareLogger.js
+++ packages/middlewares/src/middlewareLogger.js
@@ -1,6 +1,6 @@
 export function loggerMiddleware(logger) {
 	return (req, res, next) => {
-		logger.request(req.method, req.path, req.method === "GET" ? req.query : req.body, req.headers["x-request-id"], req.headers["x-correlation-id"] );
+		logger.request(req.method, req.path, req.method === "GET" ? req.query : req.body, req.get("x-request-id"), req.get("x-correlation-id") );
 		next();
 	};
 }
