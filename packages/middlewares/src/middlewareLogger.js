039938a1872d9d63060b49537e4a38be88e1b80c
Khaaz <khaaz.dev@gmail.com>
2023-03-18 01:15:02 +0100

    fix: correctly log req.query

--- packages/middlewares/src/middlewareLogger.js
+++ packages/middlewares/src/middlewareLogger.js
@@ -1,6 +1,7 @@
 export function loggerMiddleware(logger) {
+	// req.query is a `getter` so we need to copy it to a new object in order to correctly log it
 	return (req, res, next) => {
-		logger.request(req.method, req.path, req.method === "GET" ? req.query : req.body, req.get("x-request-id"), req.get("x-correlation-id") );
+		logger.request(req.method, req.path, req.method === "GET" ? { ...req.query } : req.body, req.get("x-request-id"), req.get("x-correlation-id") );
 		next();
 	};
 }
