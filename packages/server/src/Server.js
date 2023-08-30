206d0a45590ba309a85a27f2578f9d6088d67795
Khaaz <khaaz.dev@gmail.com>
2023-08-30 18:32:36 +0200

    feat(server): add health endpoint

--- packages/server/src/Server.js
+++ packages/server/src/Server.js
@@ -30,10 +30,15 @@ export class Server {
 			const { default: swaggerJsdoc } = await import("swagger-jsdoc");
 			
 			const openapiSpecification = swaggerJsdoc(getSwaggerOptions(swaggerOptions) );
-			this.logger.info("Swagger", "Initialising Swagger with config: ", openapiSpecification);
+			this.logger.info("Swagger", "Initialising Swagger with config", openapiSpecification);
 			this.app.use("/swagger", swaggerUi.serve, swaggerUi.setup(openapiSpecification) );
-			this.logger.info("Swagger", "Swagger server started!");
+			this.logger.info("Swagger", "/swagger => Swagger server started!");
 		}
+	}
+	
+	addHealthEndpoint() {
+		this.app.use("/health", (req, res) => res.status(200).send( {} ) );
+		this.logger.info("Health", "/health => Health endpoint active!");
 	}
 
 	addMiddleware(middleware) {
