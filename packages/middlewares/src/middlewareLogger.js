e47a4b7dd3f227643dcd39f147bb93d773d27aa1
Khaaz <khaaz.dev@gmail.com>
2023-11-05 23:56:59 +0100

    fix(middlewares): never log health check req

--- packages/middlewares/src/middlewareLogger.js
+++ packages/middlewares/src/middlewareLogger.js
@@ -1,7 +1,9 @@
 export function loggerMiddleware(logger) {
 	// req.query is a `getter` so we need to copy it to a new object in order to correctly log it
 	return (req, res, next) => {
-		logger.request(req.method, req.path, req.method === "GET" ? { ...req.query } : req.body, req.get("x-request-id"), req.get("x-correlation-id") );
+		if (req.path !== "/health") {
+			logger.request(req.method, req.path, req.method === "GET" ? { ...req.query } : req.body, req.get("x-request-id"), req.get("x-correlation-id") );
+		}
 		next();
 	};
 }
