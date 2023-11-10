69082b79455cfb14b3774d5a3a8817e630566928
Khaaz <khaaz.dev@gmail.com>
2023-11-11 00:30:04 +0100

    feat(server): add some generic errors

--- packages/server/src/ErrorStatusEnum.js
+++ packages/server/src/ErrorStatusEnum.js
@@ -1,7 +1,27 @@
 export const ErrorStatusEnum = {
-	GENERIC_ERROR: {
-		code: 1,
+	GENERIC: {
+		code: 0,
 		message: "Generic error",
+	},
+	UNKNOWN: {
+		code: 1,
+		message: "Unknown error",
+	},
+	DB: {
+		code: 2,
+		message: "Issue with DB",
+	},
+	EXTERNAL_API: {
+		code: 3,
+		message: "Issue when calling an external (non owlebot) API",
+	},
+	INTERNAL: {
+		code: 4,
+		message: "Issue in current API",
+	},
+	INTERNAL_CALL: {
+		code: 5,
+		message: "Issue when calling an other owlebot API",
 	},
 	
 	TWITCH_ERROR: {
